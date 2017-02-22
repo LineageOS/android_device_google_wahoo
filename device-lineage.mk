5f79452d4bee81dced09c520ab0d5fdfeaa67f69
Thierry Strudel <tstrudel@google.com>
2017-02-21 17:58:04 -0800

    kernel-headers: intial import from kernel
    Change-Id: I8c452dd2fb9ef0358ed0f06e138d0481b5cfd18a
    Signed-off-by: Thierry Strudel <tstrudel@google.com>

--- device-lineage.mk
+++ device-lineage.mk
@@ -2,6 +2,9 @@
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# Kernel headers
+PRODUCT_VENDOR_KERNEL_HEADERS := device/google/wahoo/kernel-headers
+
 # IMS
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
