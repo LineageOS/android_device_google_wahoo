268e89028ccb073c902abb9e02cdd01e75323698
Christopher N. Hesse <raymanfx@gmail.com>
2017-11-25 13:42:57 +0100

    wahoo: Compile kernel inline
    Change-Id: I59ca916dcc1ec3c5b3ff56ae76ad56db5cafbbce

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -1,1 +1,8 @@
 # Common board config for taimen, walleye
+
+# Kernel
+BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_SOURCE := kernel/google/wahoo
+TARGET_NEEDS_DTBOIMAGE := true
