43a3ebd0b4a0c5d96fa574ab8c7f944d718e6226
Christopher N. Hesse <raymanfx@gmail.com>
2017-11-25 13:42:57 +0100

    wahoo: Compile kernel inline
    Change-Id: I59ca916dcc1ec3c5b3ff56ae76ad56db5cafbbce
    
    wahoo: Remove prebuilt dtbo image definitions
    
    Change-Id: I3a46c80a657b9cd640f0d600aa2405fe962e5c7f
    
    wahoo: Remove defconfig flag
    
    This is moved into muskie/taimen trees.
    
    Change-Id: I42edf5339d13a7393db4fa28dd165a3f64ea590f
    
    wahoo: Build device tree overlay image
    
    Change-Id: Ia81db83b24286cbd2b4aaacb09a085d304f5c50a
    
    wahoo: Compile kernel with clang
    
    Change-Id: I5c4aef7d63dc8925223833a088cb51e57c59146d
    
    wahoo: Build Image.lz4-dtb to match stock
    
    Change-Id: I1f5576364e0bdeac4a26c203b43835bc57c57429
    
    wahoo: Define paths to DTC and MKDTIMG
    
    These are no longer on PATH, so we have to define them explicitly
    for the kernel make system to find them.
    
    Change-Id: I69f1a01f2f3d4c90e5a2337f44ee8e1bcb1cc1e8

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -1,3 +1,13 @@
 # Common board config for taimen, walleye
 
+# Kernel
+BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
+    MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_SOURCE := kernel/google/wahoo
+TARGET_NEEDS_DTBOIMAGE := true
+
 -include vendor/google/wahoo/BoardConfigVendor.mk
