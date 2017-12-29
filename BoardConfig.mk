d0e1e285304a308237d71ae5baee8c03074a4e65
Abhisek Devkota <ciwrl@lineageos.org>
2017-12-28 21:24:46 -0800

    [DNM] Wahoo: Selinux Permissive
    Change-Id: I76e3993741e05e3ace93de8d16edcc438e3c525c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,6 +38,7 @@ BOARD_KERNEL_CMDLINE += swiotlb=2048
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += raid=noautodetect
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
