b0f6951ddc43935fb84ffc3ac6f0cd0477dd673d
Abhisek Devkota <ciwrl@lineageos.org>
2018-01-04 21:19:09 -0800

    Enable EUICC Sim
    Change-Id: If43e859c80e69b35b08c1097ad268cf4411d2d8b

--- device-lineage.mk
+++ device-lineage.mk
@@ -3,3 +3,7 @@ PRODUCT_PACKAGES += libion
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/wahoo/overlay-lineage
+
+# EUICC feature
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.euicc.xml
