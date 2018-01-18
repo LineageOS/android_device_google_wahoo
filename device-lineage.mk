1336aee00fc22d7283eba7bdc41678af856dd4f8
Abhisek Devkota <ciwrl@lineageos.org>
2018-01-17 21:53:21 -0800

    wahoo: IMS Support
    Build what we can
    
    Change-Id: I9e482b9c35131e3634aaf7a6c8ba05ade11b28fb

--- device-lineage.mk
+++ device-lineage.mk
@@ -2,6 +2,11 @@
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# IMS
+PRODUCT_PACKAGES += \
+    PresencePolling \
+    RcsService
+
 # Shared java libs
 PRODUCT_PACKAGES += \
     com.android.nfc_extras
