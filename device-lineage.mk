b7c1a57ec80c2387e674984d1809ccbf55dc6859
razorloves <razorloves@gmail.com>
2018-09-02 23:30:11 -0600

    wahoo: Set OPA property to enable google assistant
    Change-Id: I741171219d64a28d56531f063f95f3f6f6cf81e6

--- device-lineage.mk
+++ device-lineage.mk
@@ -2,6 +2,10 @@
 PRODUCT_PACKAGES += \
     Snap
 
+# Enable missing props
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.opa.eligible_device=true
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
