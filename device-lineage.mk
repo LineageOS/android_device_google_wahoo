03c21180b10406498edbf12d9a8b7c9d1a01f38c
Joey <joey@lineageos.org>
2019-01-13 10:21:11 +0100

    wahoo: Build Trust HAL
    Change-Id: I92706987f8d3094cdfb82e71e5331b7efe69e5df

--- device-lineage.mk
+++ device-lineage.mk
@@ -12,3 +12,7 @@ DEVICE_PACKAGE_OVERLAYS += device/google/wahoo/overlay-lineage
 # Parts
 PRODUCT_PACKAGES += \
     GoogleParts
+
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
