2815d80b4096ccb72437f857dfa8c98be71637c0
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-07 11:13:09 -0700

    wahoo: Build a disabled vbmeta image
    Change-Id: Ie8f1e0929b037b35b5e45c2e31be9253fa0bb097

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -13,4 +13,8 @@ TARGET_KERNEL_SOURCE := kernel/google/wahoo
 TARGET_NEEDS_DTBOIMAGE := true
 BOARD_CUSTOM_DTBOIMG_MK := $(WAHOO_PATH)/kernel/dtbo.mk
 
+# Verified Boot
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+
 -include vendor/google/wahoo/BoardConfigVendor.mk
