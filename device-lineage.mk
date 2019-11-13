063346063cb11a8314623f231274ba2502872996
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-13 12:00:28 -0600

    wahoo: enable snap and configure overlay
    Change-Id: Ieda6914c95d877842933d6df463a9f475f24ee21

--- device-lineage.mk
+++ device-lineage.mk
@@ -1,3 +1,7 @@
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
