b86acce10ebc03c70fec38a16bd601c8389e507c
Paul Keith <javelinanddart@gmail.com>
2020-03-15 18:51:05 -0700

    wahoo: Build dtbo with cfg files
    * make dtbo.img doesn't want to work for some reason,
      so we'll do it the "CAF" way and compile the dtbo.img
      separately using mkdtimg out of tree
    * We use a custom dtbo.mk because mkdtimg with create
      and a list of dtbo files doesn't work for wahoo, so
      we use cfg_create with BOARD_CUSTOM_DTBOIMG_CFG
    * If BOARD_CUSTOM_DTBOIMG_CFG isn't set by the device,
      we'll use the one in the kernel tree from Google.
      This isn't the desired method because the current
      taimen and walleye defconfigs only build the dtbo
      files for themselves, but Google's config expects
      dtbo files for both devices to be present
    
    Change-Id: I70cdab278ab20bcf7a175fc1faa4556d0bec0a5f

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -3,11 +3,13 @@
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_ADDITIONAL_FLAGS := \
-    DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
-    MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/google/wahoo
-TARGET_NEEDS_DTBOIMAGE := true
+
+# DTBO
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_CUSTOM_DTBOIMG_MK := device/google/wahoo/kernel/dtbo.mk
 
 -include vendor/google/wahoo/BoardConfigVendor.mk
