96059548e60d6e08e2a31794aa54d1aa7c47df69
Eamon Powell <eamonpowell@outlook.com>
2020-03-17 18:15:13 +1100

    wahoo: Add missing properties and packages
    Now that we are building our own vendor image, some necessary packages aren't being built and some props aren't being set.
    
    Change-Id: I3e91822b336761cdecf3f6ebe416276dc0585785

--- device-lineage.mk
+++ device-lineage.mk
@@ -30,3 +30,20 @@ AB_OTA_PARTITIONS += \
 # ipacm configuration files
 PRODUCT_PACKAGES += \
     IPACM_cfg.xml
+
+# Build necessary packages for vendor
+PRODUCT_PACKAGES += \
+    ese-ls-provision \
+    ese-replay \
+    libjson \
+    libsensorndkbridge:32 \
+    libtinyxml
+
+# Add missing vendor props
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true \
+    media.mediadrmservice.enable=true \
+    ro.oem_unlock.pst_offset=6144 \
+    ro.oem_unlock.pst=/dev/block/platform/soc/1da4000.ufshc/by-name/misc \
+    ro.hardware.egl=adreno \
+    ro.gfx.driver.0=com.google.pixel.wahoo.gfxdrv
