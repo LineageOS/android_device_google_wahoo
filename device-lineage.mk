fc2ec176b963562ae569cfe4354c45b29acbd23c
Dan Pasanen <dan.pasanen@gmail.com>
2020-03-31 10:31:06 +1100

    wahoo: enable snap and configure overlay
    Change-Id: Ieda6914c95d877842933d6df463a9f475f24ee21

--- device-lineage.mk
+++ device-lineage.mk
@@ -11,3 +11,10 @@ PRODUCT_PACKAGES += \
 # Enable missing props
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.opa.eligible_device=true
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += device/google/wahoo/overlay-lineage
+
+# Snap
+PRODUCT_PACKAGES += \
+    Snap
