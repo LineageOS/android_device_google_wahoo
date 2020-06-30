61ddbd83e22333a7288ffd899044b995a1f5161f
Eamon Powell <eamonpowell@outlook.com>
2020-06-30 20:31:06 +1000

    wahoo: Drop unused include
    Change-Id: Ifcc604303f1659339ab655bca4492edba45a3f80

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -9,5 +9,3 @@ TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/google/wahoo
 TARGET_NEEDS_DTBOIMAGE := true
-
--include vendor/google/wahoo/BoardConfigVendor.mk
