26a0813e9f5ad719debee4ce0b4737aefdbacea8
Steven Moreland <smoreland@google.com>
2021-05-26 00:41:38 +0000

    wahoo: Move /vendor/ueventd.rc to /vendor/etc/ueventd.rc
    The root location of this file is deprecated for Android T.
    
    Bug: 189268918
    Bug: 178221726
    Test: device reaches boot complete after raising API level to 32
    Change-Id: I97a6d2e5cacd0ac5eeb81daafeb8b1a0ad5c119b

--- device.mk
+++ device.mk
@@ -63,7 +63,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init.recovery.hardware.rc:recovery/root/init.recovery.$(PRODUCT_HARDWARE).rc \
     $(LOCAL_PATH)/init.hardware.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.wahoo.usb.rc \
-    $(LOCAL_PATH)/ueventd.hardware.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc \
+    $(LOCAL_PATH)/ueventd.hardware.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc \
     $(LOCAL_PATH)/init.elabel.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/init.elabel.sh \
     $(LOCAL_PATH)/init.power.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.power.sh \
     $(LOCAL_PATH)/init.radio.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.radio.sh \
