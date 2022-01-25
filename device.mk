4e5269525b7f762187bc023daf72ec93407edc6a
Josh Wu <joshwu@google.com>
2022-01-24 22:56:14 -0800

    wahoo: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7

--- device.mk
+++ device.mk
@@ -494,8 +494,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.soundtrigger@2.2-impl:32 \
     android.hardware.bluetooth.a2dp@1.0.vendor \
-    android.hardware.bluetooth.audio@2.1-impl \
-    android.hardware.bluetooth.audio@2.1.vendor \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.audio@2.0-service
 
 # stereo speakers: orientation changes swap L/R channels
