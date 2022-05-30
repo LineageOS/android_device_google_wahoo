c8696985e9fd9902db23662d37597d29499593c6
Bruno Martins <bgcngm@gmail.com>
2022-05-30 19:27:51 +0100

    wahoo: Drop default DRM service
    This is not needed, since Widevine DRM and Clearkey DRM
    services are available.
    
    Change-Id: I97023f1bab88ddb8fa4cb205df190c516a759951

--- device.mk
+++ device.mk
@@ -386,8 +386,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.0-impl:32 \
-    android.hardware.drm@1.0-service \
     android.hardware.drm@1.4-service.clearkey
 
 # NFC packages
