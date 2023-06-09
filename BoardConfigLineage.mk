3042373627ab6d77105acb5dc649f34222e62f98
Nolen Johnson <johnsonnolen@gmail.com>
2023-06-09 16:21:00 -0400

    wahoo: Allow toggling charging
    * We're better off than newer pixels here, we use smb2.
    
    Reference: Id08c169f0c507390eab070d1ae77bfb992b50b81
    Change-Id: Ia4301489dae31a60f31707e40dbb7bfddfd14058

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -1,8 +1,5 @@
 # Common board config for taimen, walleye
 
-# Lineage Health
-TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE := false
-
 # Manifests
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 
