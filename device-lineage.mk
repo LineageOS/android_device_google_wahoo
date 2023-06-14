77877c9af56d9162223c7c1e15758feb8769a6da
Nolen Johnson <johnsonnolen@gmail.com>
2023-06-14 13:41:25 -0400

    [TMP] Revert "wahoo: Build Lineage Health HAL"
    This reverts commit 5b52500ff1e3a7530abb45ca5ab8691262c76bf8.
    
    Reason: Something is wrong with the toggle - additional time needed.
    Change-Id: If493d3785f454b97438f6bce4518afb0fd516405

--- device-lineage.mk
+++ device-lineage.mk
@@ -7,9 +7,6 @@ ifeq ($(WITH_GMS),true)
 GMS_MAKEFILE=gms_minimal.mk
 endif
 
-# Lineage Health
-include hardware/google/pixel/lineage_health/device.mk
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/wahoo/overlay-lineage
 
