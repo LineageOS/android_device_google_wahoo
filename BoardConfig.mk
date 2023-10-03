0b243195a1319fff0f1797da0c32b7ab53e21b2c
Cole Faust <colefaust@google.com>
2023-10-03 15:51:10 -0700

    wahoo: Add BUILD_BROKEN_INCORRECT_PARTITION_IMAGES
    Qcom has issues that prevent it from working with a hermetic
    partition build.
    
    Bug: 205632228
    Test: Presubmits
    Change-Id: I490a29da38a6f68e7dd3650d8140cb11adaa8c7a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,6 +30,7 @@ TARGET_2ND_CPU_VARIANT := cortex-a73
 
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_INCORRECT_PARTITION_IMAGES := true
 
 BOARD_KERNEL_CMDLINE += androidboot.hardware=$(TARGET_BOOTLOADER_BOARD_NAME) androidboot.console=ttyMSM0 lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
