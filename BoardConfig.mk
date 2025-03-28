3e30d80d625e03a6cecd643abae1a5d9b7c31e54
Michael Bestas <mkbestas@lineageos.org>
2025-03-28 20:37:55 +0200

    wahoo: Enforce sysprop owner
    Change-Id: Ic6b6ef023eebab84d270533bdc35d555fc923f24

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,7 +184,5 @@ AB_OTA_PARTITIONS += \
     dtbo \
     vendor
 
-BUILD_BROKEN_ENFORCE_SYSPROP_OWNER := true
-
 # Allow Lineage config to override others
 -include device/google/wahoo/BoardConfigLineage.mk
