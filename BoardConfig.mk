fcfe7646802407c10213f8a251645c99d0a51539
Nolen Johnson <johnsonnolen@gmail.com>
2025-04-02 00:43:36 -0400

    wahoo: Fold Lineage specific makefiles into main ones
    * We can't build AOSP targets anymore, we rely on platform
      stuff.
    
    Change-Id: I6e4b1b4e6b1abd39cd1f72bd03cbdef1ff1a59f1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,7 +170,9 @@ TARGET_VENDOR_PROP := $(LOCAL_PATH)/vendor.prop
 DEVICE_MANIFEST_FILE := device/google/wahoo/manifest.xml
 DEVICE_MATRIX_FILE := device/google/wahoo/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := device/google/wahoo/framework_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := device/google/wahoo/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    device/google/wahoo/device_framework_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 
 BOARD_VNDK_VERSION := current
@@ -184,6 +186,3 @@ AB_OTA_PARTITIONS += \
     vbmeta \
     dtbo \
     vendor
-
-# Allow Lineage config to override others
--include device/google/wahoo/BoardConfigLineage.mk
